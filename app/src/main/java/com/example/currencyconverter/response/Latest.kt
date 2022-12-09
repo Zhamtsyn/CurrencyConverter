62cbf694051174e88bd33cecbe51cc28f71b6abb
punnuq <renatazh456@gmail.com>
2022-12-09 15:38:11 +0200

    change api call

--- app/src/main/java/com/example/currencyconverter/response/Latest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.currencyconverter.response
-
-data class Latest(
-    val base: String,
-    val date: String,
-    val rates: Rates,
-    val success: Boolean,
-    val timestamp: Int
-)
--- app/src/main/java/com/example/currencyconverter/response/Symbols.kt
+++
@@ -1,6 +0,0 @@
-package com.example.currencyconverter.response
-
-data class Symbols(
-    val success: Boolean,
-    val symbols: SymbolsX
-)
